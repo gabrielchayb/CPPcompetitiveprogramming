5097155de89b241138946ce3f309c2d19f15fd0b
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-11-05 16:10:18 -0300

    questao 2 elaine

--- treino.c
+++ treino.c
@@ -1,33 +1,18 @@
 #include <stdio.h>
 
-/*(8 pontos) Faça uma função chamada maxpot() que recebe como parâmetros três
-inteiros: X, Y e MAX. Esta função deve devolver 2 valores:
-- O resultado de XY
-, SEM usar a função pronta pow (exponenciação) da linguagem c;
-- O valor 1 (um) se resultado de XY
-for maior que MAX, ou 0 (zero) caso contrário
-*/
-
-int maxpot(int x, int y, int max){
-      int i, resultado = 1;
-
-      for(i = 0; i < y; i++){
-         resultado = x * resultado;
-      }
-
-      if(resultado > max){
-         return 1;
-      }
-      
-      else{
-         return 0;
-      }
+int cont(int N){
+   if (N < 10){
+      return 1;
+   }
+   else{
+      return 1 + cont(N / 10);
+   }
 }
-
 int main(){
-   int x, y, max;
-   scanf("%d %d %d", &x, &y, &max);
-   printf("%d\n", maxpot(x, y, max));
-   return 0;
+   int N; //432
+   scanf("%d", &N);
+   int qtd = cont(N);
+   printf("%d", qtd);
+
 }
 
