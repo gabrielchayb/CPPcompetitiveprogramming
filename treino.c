9bf8bec34c0f1136b2a7bc33291c0dbc50fb8d74
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-11-05 16:15:38 -0300

    questao 3 elaine

--- treino.c
+++ treino.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 
-int cont(int N){
-   if (N < 10){
-      return 1;
-   }
-   else{
-      return 1 + cont(N / 10);
-   }
-}
-int main(){
-   int N; //432
-   scanf("%d", &N);
-   int qtd = cont(N);
-   printf("%d", qtd);
+/*Escrever a função recursiva int max(int n, int *v) que retorna o maior
+elemento do vetor v (de n elementos). 
+*/
 
+int max(int n, int *v){ // n = 5, v = {1, 2, 3, 4, 5}
+      if(n == 1){
+         return v[0]; // 1
+      }
+
+      int x = max(n-1, v); // x = 4
+
+      if(x > v[n-1]){   // 4 > 5
+         return x;
+      }
+
+      return v[n-1];    // 5
+}
+
+int main(){
+   int v[5] = {1, 2, 3, 4, 5};
+   printf("%d\n", max(5, v));
+   return 0;
 }
 
