a439a7ad04aabb0a776d91dc3a8f91ec4bdc1fdb
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2023-11-06 11:29:43 -0300

    TRABALHO 4 - ELAINE

--- treino.c
+++ treino.c
@@ -1,85 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define SMAX 505
-
-char m[60][60];
-int tam1,tam2;
-
-void build(){
-    char i='A',j='A',value='A';
-    //i = linha, j = coluna, value = valor que mudará ao decorrer da construção da tabula rasa
-    while(i!='z' || j!='z'){
-        m[i-'A'][j-'A'] = value;
-        if(j=='z'){
-            j = 'A';
-            //se value for maiusculo, como chegou na última coluna, basta mudar duas vezes, então A não vira a, mas sim B, logo é só somar +1
-            if(value>='A' && value<='Z'){
-                value++;
-                if(value>'Z') value -= 26; //se ultrapassar, subtrai 26 (letras do alfabeto)
-            }
-            //se value for minusculo, como chegou na última coluna, basta mudar duas vezes, então A não vira a, mas sim B, logo é só somar +1
-            else{
-                value++;
-                if(value>'z') value -= 26; //se ultrapassar, subtrai 26 (letras do alfabeto)
-            }
-            //se for letra maiuscula, transforma essa letra em minuscula (só somar 32)
-            if(i>='A' && i<='Z') i += 32;
-            //se for letra minuscula, volto para maiuscula (decrementa 32) e soma mais 1, para ir para a letra da frente
-            else i -= 31;
-        }
-        else{
-            if(value=='z') value = 'A';
-            else{
-                if(value>='A' && value<='Z') value += 32;
-                else value -= 31;
-            }
-            //se for letra maiuscula, transforma essa letra em minuscula (só somar 32)
-            if(j>='A' && j<='Z') j += 32;
-            //se for letra minuscula, volto para maiuscula (decrementa 32) e soma mais 1, para ir para a letra da frente
-            else j -= 31;
-        }
-    } m[i-'A'][j-'A'] = value;
-}
-
-char letra_cifra(char ch1, char ch2){ //recebe o caractere da mensagem e o caractere da palavra-chave
-    char resp; //caractere resposta, atribuo o que esta salvo na tabula rasa nele e retorno
-    resp = m[ch1-'A'][ch2-'A'];
-    return resp;
-}
-
-char *cifra(char *str1, char *str2){
-    char *novo; //ponteiro char que é alocado dinamicamente e guardará a mensagem cifrada
-    novo = (char *) malloc(tam1*sizeof(char));
-    int b=0,c=0; //b representa as posições da str2 (a palavra-chave), ao decorrer do for é incrementado, mas se ultrapassar do tamanho da string, volta para 0
-    //c representa as posições de *novo, ao decorrer do for é incrementado
-    for(int a=0;a<tam1;a++){
-        if(str1[a]!=' '){ //se não for um espaço, é chamado a função letra_cifra para pegar o caractere cifrado
-            novo[c++] = letra_cifra(str1[a],str2[b++]);
-            if(b==tam2) b=0;
-        }
-        else novo[c++] = ' '; //se for espaço, essa posição da string será apenas um espaço
-    }
-    return novo;
-}
+#include <math.h>
 
 int main(){
-    char *str1,*str2,*str3; 
-    //alocação dinamica de duas strings
-    str1 = (char *) malloc(SMAX*sizeof(char));
-    str2 = (char *) malloc(SMAX*sizeof(char));
-    //leitura de duas strings
-    gets(str1);
-    gets(str2);
-    //o tamanho de cada string é salvo em variaveis int
-    tam1 = strlen(str1);
-    tam2 = strlen(str2);
-    //função build é chamada, a qual constroi a tabula rasa
-    build();
-    //o ponteiro str3 recebe o que será retornado da função
-    str3 = cifra(str1,str2);
-    //mensagem cifrada resultante é impressa
-    for(int i=0;i<tam1;i++) printf("%c",str3[i]);
-    //memoria é liberada
-    free(str3); free(str2); free(str1);
+    int soma_palavras=0,soma_paragrafos=0,review_count=0,mudanca=1;
+    int tamanho_menor_palavra=INT_MAX; //valor inicialmente muito grande
+    int tamanho_maior_palavra=-INT_MAX; //valor inicialmente muito pequeno
+    char *menor_palavra,*maior_palavra;
+    menor_palavra = (char *) malloc(500);
+    maior_palavra = (char *) malloc(500);
+    char excecao_palavra[] = " .,;-\n\t()!/\""; //todos os caracteres que não permanecem a composição de uma palavra
+    char nome_arquivo[] = "..\\trabalho4\\rev1.bin";
+    FILE *p;
+    while(1){
+        nome_arquivo[16] = mudanca+'0'; mudanca++; //de rev1 passa para rev2 e assim por diante
+        p = fopen(nome_arquivo,"rb"); //abre o arquivo
+        if(p==NULL){ //se o arquivo não existe (ou se deu erro), acaba o programa
+            break;
+        }
+
+
+        review_count++; //contador de arquivos analisados
+        char *review_completa = (char *) malloc(5000); 
+        int pos=0;
+        char c;
+        while((c = fgetc(p))!=EOF){ //inicialmente guardamos a review completa em uma string de até 5000 caracteres
+            review_completa[pos] = c;
+            if(pos!=0 && review_completa[pos]=='\t' && review_completa[pos-1]=='\n'){ //se há um \n seguido de um \t, é um paragrafo
+                soma_paragrafos++;
+            }
+            pos++; //passa para a proxima posição que sera guardado na string review_completa
+        } review_completa[pos] = '\0'; //fecha a string com \0
+
+
+        char *palavra = strtok(review_completa,excecao_palavra); //guarda a primeira palavra da review
+        while(palavra!=NULL){ //se realmente existe uma palavra
+            soma_palavras++;
+            int tam = strlen(palavra);
+            if(tam < tamanho_menor_palavra){ //se o tamanho dessa palavra for menor do que o tamanho da menor palavra, guardo a nova palavra
+                tamanho_menor_palavra = tam;
+                strcpy(menor_palavra,palavra);
+            }
+            if(tam > tamanho_maior_palavra){ //se o tamanho dessa palavra for maior do que o tamanho da maior palavra, guardo a nova palavra
+                tamanho_maior_palavra = tam;
+                strcpy(maior_palavra,palavra);
+            }
+            palavra = strtok(NULL,excecao_palavra); //passa para a próxima palavra da review
+        }
+        free(review_completa); //libera a memória
+        fclose(p); //fecha o arquivo
+    }
+    p = fopen("..\\trabalho4\\relatorio.out","w"); //arquivo "relatorio.out" é aberto para ser escrito as informações da analise dentro dele
+    if(p==NULL){
+        printf("Erro.");
+        exit(1);
+    }
+    double media_palavras = (soma_palavras*1.0)/review_count;
+    fprintf(p,"1) Media do numero de palavras por review: %.1lf\n",media_palavras);
+    double media_paragrafos = (soma_paragrafos*1.0)/review_count;
+    fprintf(p,"2) Media do numero de paragrafos por review: %.1lf\n",media_paragrafos);
+    fprintf(p,"3) Maior palavra: %s\n",maior_palavra);
+    fprintf(p,"4) Menor palavra: %s\n",menor_palavra);
+    fclose(p);
+    exit(1);
 }
