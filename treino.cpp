fbb89ce14277c813b739bd6cf7a06d19c49bca1f
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2024-01-11 11:25:59 -0300

    busca inversa no vetor normal

--- treino.cpp
+++ treino.cpp
@@ -3,34 +3,13 @@ using namespace std ;
 
 int main() {
     int v[10];
-    int X;
-    int cont = 0;
-    vector<int> pos;
 
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0; i < 10; i++){
         cin >> v[i];
     }
 
-    cin >> X;
-
-    for(int i = 0; i < 10; i++) {
-        if(v[i] == X) {
-            cont++;
-            pos.push_back(i);
-
-        }
+    for(int i = 9; i >= 0; i--){
+        cout << v[i] << " ";
     }
-
-    if(cont == 0) {
-        cout << "Mia x" << endl;
-    }
-    
-    else{
-        cout << cont << endl;
-        for(int i = 0; i < pos.size(); i++){
-            cout << pos[i] << " ";
-        }
-    }
-    return 0;
 }
 
