cec21502812386ac81651c19b02679988894633c
Gabriel Ribeiro <95705080+gabrielchayb@users.noreply.github.com>
2024-01-11 11:19:29 -0300

    Busca simples no vetor 02 - USANDO VECTOR
    Faça um programa para ler 10 valores inteiros, armazená-los em um vetor, depois ler um inteiro
    �
    X. Se
    �
    X aparece no vetor, imprima quantas vezes ele aparece e depois quais os índices que ele aparece. Se
    �
    X não aparecer no vetor, imprima apenas a mensagem "Mia x". Lembre-se que a primeira posição de um vetor tem índice 0.
    
    Entrada
    A primeira linha da entrada consiste de 10 inteiros separados por um espaço em branco. A segunda linha contém um inteiro
    �
    X.
    
    Saída
    Se
    �
    X aparece no vetor, a saída consiste de duas linhas, a primeira linha contém um inteiro representando a quantidade de vezes que
    �
    X apareceu no vetor e a segunda linha contém os índices do vetor que contém valor igual ao
    �
    X. Se
    �
    X não aparecer no vetor, imprima apenas a mensagem "Mia x".

--- treino.cpp
+++ treino.cpp
@@ -1,9 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std ;
 
-
 int main() {
+    int v[10];
+    int X;
+    int cont = 0;
+    vector<int> pos;
+
+    for(int i = 0; i < 10; i++) {
+        cin >> v[i];
+    }
+
+    cin >> X;
+
+    for(int i = 0; i < 10; i++) {
+        if(v[i] == X) {
+            cont++;
+            pos.push_back(i);
+
+        }
+    }
+
+    if(cont == 0) {
+        cout << "Mia x" << endl;
+    }
     
-    
+    else{
+        cout << cont << endl;
+        for(int i = 0; i < pos.size(); i++){
+            cout << pos[i] << " ";
+        }
+    }
     return 0;
 }
+
